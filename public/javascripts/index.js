fc5f14a207b8bee93d7a3e7368a009363a9e353b
Jibbajabba <Jibbajabbafic@users.noreply.github.com>
2018-03-27 22:16:57 +0100

    Cleaned up directory by removing and renaming index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,29 +1,65 @@
-// ----- General Functions -----
+// ------------------------------ General Functions ------------------------------
 
-var MAX_DATAPOINTS = 1;
+var MAX_DATAPOINTS_BASIC = 1;
+var MAX_DATAPOINTS_ADV = 50;
 var energyTemp = 0;
 
-// Array to hold our data
-var electricStats = {
-    voltageArry: [],
-    currentArry: [],
-    powerArry: [],
-    energyArry: []
+var statList = [
+    {
+        name: "bike", 
+        voltageArry: [],
+        currentArry: [],
+        powerArry: [],
+        powerArryBasic: [],
+        energyArry: [],
+        rpmArry: []
+    },
+    {
+        name: "handcrank", 
+        voltageArry: [],
+        currentArry: [],
+        powerArry: [],
+        powerArryBasic: [],
+        energyArry: [],
+        rpmArry: []
+    },
+];
+
+// var updateHolders = function(){};
+var updateStats = function(){};
+
+function searchList(list, property, value) {
+    for (var i = 0; i < list.length; i++) {
+        if (list[i][property] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Function to specified tab
+function changeTab(tabID) {
+	// Hide all tabs
+    $(".tab").hide();
+    $("button.nav").prop('disabled', false);
+    
+    // Show specified tab with id = tabID
+    $("button.nav#" + tabID).prop('disabled', true);
+	$(".tab#" + tabID).show();
 };
 
-var updateHolders = function(){};
+// ------------------------------ Socket.io Code ------------------------------
 
-// ----- Socket.io Code -----
 // Define socket which automatically uses the connected server
 var socket = io();
 
 // Define what to do when stats are received
 socket.on('stats', function(data) {
-    // var parsedData = data[0];
     console.log('Received stats: ');
     console.log(data);
-    // pushStats(electricStats, 'power', parsedData);
-    updateHolders(data, electricStats);
+
+    // updateHolders(data, electricStats);
+    updateStats(data);
 });
 
 // Code to update button colour based on if reading sensor
@@ -35,19 +71,33 @@ socket.on('toggleStatus', function(data){
     }
 });
 
-// ----- jQuery Functions -----
+// ------------------------------ jQuery Functions ------------------------------
+
 $(document).ready( function() {
-    // When clicked emits a 'toggle' event
-    $('#toggleRead').on('click', function(){
+    
+    // ----- Button Code -----
+    
+    $('button#toggleRead').on('click', function(){
         console.log('TOGGLING');
         socket.emit('toggle', '');
     });
-    // ----- CanvasJS stuff -----
 
-    // Create new charts to hold our data
-    var barChartPower = new CanvasJS.Chart("powerChart", {
+    $('button#basic').on('click', function(){
+        console.log('Switching to basic view');
+        changeTab("basic");
+    });
+
+    $('button#advanced').on('click', function(){
+        console.log('Switching to advanced view');
+        changeTab("advanced");
+    });
+
+    // ------------------------------ CanvasJS stuff ------------------------------
+
+    // --------------- Basic view charts ---------------
+    var basicChartPowerOptions1 = {
         title: {
-            text: "Power Output"
+            text: "Power Output of " + statList[0].name
         },
         axisY: {
             title: "Power (W)",
@@ -82,6 +132,12 @@ $(document).ready( function() {
                     showOnTop: true
                 },
                 {                
+                    value: 250,
+                    color: '#ffc400',
+                    label: "Travel Kettle",
+                    showOnTop: true
+                },                
+                {                
                     value: 700,
                     color: '#ffc400',
                     label: "Toaster",
@@ -94,13 +150,131 @@ $(document).ready( function() {
             type: "bar",
             xValueType: "dateTime",
             xValueFormatString: "HH:mm:ss.fff",
-            dataPoints: electricStats.powerArry[0]
+            dataPoints: statList[0].powerArryBasic
+        }]
+    };
+
+    var basicChartPowerOptions2 = {
+        title: {
+            text: "Power Output of " + statList[1].name
+        },
+        axisY: {
+            title: "Power (W)",
+            suffix: "W",
+            viewportMinimum: 0,
+            viewportMaximum: 1000,
+            animationEnabled: true,
+            animationDuration: 5000,
+            stripLines:[
+                {                
+                    value: 10,
+                    color: '#ffc400',
+                    label: "LED Light",
+                    showOnTop: true
+                },
+                {                
+                    value: 50,
+                    color: '#ffc400',
+                    label: "Fluorescent Light",
+                    showOnTop: true
+                },
+                {                
+                    value: 100,
+                    color: '#ffc400',
+                    label: "Incandescent Light",
+                    showOnTop: true
+                },
+                {                
+                    value: 200,
+                    color: '#ffc400',
+                    label: "Fan",
+                    showOnTop: true
+                },
+                {                
+                    value: 250,
+                    color: '#ffc400',
+                    label: "Travel Kettle",
+                    showOnTop: true
+                },                
+                {                
+                    value: 700,
+                    color: '#ffc400',
+                    label: "Toaster",
+                    showOnTop: true
+                }
+
+            ]
+        },
+        data: [{
+            type: "bar",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss.fff",
+            dataPoints: statList[1].powerArryBasic
+        }]
+    };
+
+    var chartBasic1 = new CanvasJS.Chart("chartContainerPower1", basicChartPowerOptions1);
+    var chartBasic2 = new CanvasJS.Chart("chartContainerPower2", basicChartPowerOptions2);
+
+    // --------------- Advanced view charts ---------------
+
+    var chartAdv1 = new CanvasJS.Chart("chartContainer1", {
+        title: {
+            text: "Power Output of " + statList[0].name
+        },
+        axisX: {
+            title: "Time (s)"
+        },
+        axisY: {
+            title: "Power (W)"
+        },
+        data: [{
+            type: "spline",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss",
+            dataPoints: statList[0].powerArry
         }]
     });
 
-    // var barChart2 = new CanvasJS.Chart("barChart2", {
+    var chartAdv2 = new CanvasJS.Chart("chartContainer2", {
+        title: {
+            text: "Voltage Output of " + statList[0].name
+        },
+        axisX: {
+            title: "Time (s)"
+        },
+        axisY: {
+            title: "Voltage (V)"
+        },
+        data: [{
+            type: "spline",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss",
+            dataPoints: statList[0].voltageArry
+        }]
+    });
+
+    var chartAdv3 = new CanvasJS.Chart("chartContainer3", {
+        title: {
+            text: "Current Output of " + statList[0].name
+        },
+        axisX: {
+            title: "Time (s)"
+        },
+        axisY: {
+            title: "Current (A)"
+        },
+        data: [{
+            type: "spline",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss",
+            dataPoints: statList[0].currentArry
+        }]
+    });
+
+    // var chartAdv4 = new CanvasJS.Chart("chartContainer4", {
     //     title: {
-    //         text: "Cumulative Energy"
+    //         text: "Cumulative Energy of " + statList[0].name
     //     },
     //     axisX: {
     //         title: "Time (s)"
@@ -109,56 +283,197 @@ $(document).ready( function() {
     //         title: "Energy (J)"
     //     },
     //     data: [{
-    //         showInLegend: true,
-    //         type: "column",
-    //         dataPoints: electricStats.energyArry
+    //         type: "spline",
+    //         xValueType: "dateTime",
+    //         xValueFormatString: "HH:mm:ss",
+    //         dataPoints: statList[0].energyArry
     //     }]
     // });
 
+    var chartAdv4 = new CanvasJS.Chart("chartContainer4", {
+        title: {
+            text: "Speed of " + statList[0].name
+        },
+        axisX: {
+            title: "Time (s)"
+        },
+        axisY: {
+            title: "Speed (RPM)"
+        },
+        data: [{
+            type: "spline",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss",
+            dataPoints: statList[0].rpmArry
+        }]
+    });
+
+// ---------- NEXT INPUT ----------
+
+    var chartAdv5 = new CanvasJS.Chart("chartContainer5", {
+        title: {
+            text: "Power Output of " + statList[1].name
+        },
+        axisX: {
+            title: "Time (s)"
+        },
+        axisY: {
+            title: "Power (W)"
+        },
+        data: [{
+            type: "spline",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss",
+            dataPoints: statList[1].powerArry
+        }]
+    });
+
+    var chartAdv6 = new CanvasJS.Chart("chartContainer6", {
+        title: {
+            text: "Voltage Output of " + statList[1].name
+        },
+        axisX: {
+            title: "Time (s)"
+        },
+        axisY: {
+            title: "Voltage (V)"
+        },
+        data: [{
+            type: "spline",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss",
+            dataPoints: statList[1].voltageArry
+        }]
+    });
+
+    var chartAdv7 = new CanvasJS.Chart("chartContainer7", {
+        title: {
+            text: "Current Output of " + statList[1].name
+        },
+        axisX: {
+            title: "Time (s)"
+        },
+        axisY: {
+            title: "Current (A)"
+        },
+        data: [{
+            type: "spline",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss",
+            dataPoints: statList[1].currentArry
+        }]
+    });
+
+    // var chartAdv4 = new CanvasJS.Chart("chartContainer8", {
+    //     title: {
+    //         text: "Cumulative Energy of " + statList[1].name
+    //     },
+    //     axisX: {
+    //         title: "Time (s)"
+    //     },
+    //     axisY: {
+    //         title: "Energy (J)"
+    //     },
+    //     data: [{
+    //         type: "spline",
+    //         xValueType: "dateTime",
+    //         xValueFormatString: "HH:mm:ss",
+    //         dataPoints: statList[1].energyArry
+    //     }]
+    // });
+
+    var chartAdv8 = new CanvasJS.Chart("chartContainer8", {
+        title: {
+            text: "Speed of " + statList[1].name
+        },
+        axisX: {
+            title: "Time (s)"
+        },
+        axisY: {
+            title: "Speed (RPM)"
+        },
+        data: [{
+            type: "spline",
+            xValueType: "dateTime",
+            xValueFormatString: "HH:mm:ss",
+            dataPoints: statList[1].rpmArry
+        }]
+    });
+
+    // Function to render all charts on the page easily
     var renderAllCharts = function() {
-        barChartPower.render();
-        // barChart2.render();
+        // Render basic chart
+        chartBasic1.render();
+        chartBasic2.render();
+
+        // Render advanced charts
+        chartAdv1.render();
+        chartAdv2.render();
+        chartAdv3.render();
+        chartAdv4.render();
+
+        chartAdv5.render();
+        chartAdv6.render();
+        chartAdv7.render();
+        chartAdv8.render();
     }
 
-    var updateStatArry = function(statObj, statArry, labelStr, xVal, yVal) {
+    // Function to update an array of stats and ensure it doesn't go over the datapoint limit
+    var updateStatArry = function(statObj, statArry, xVal, yVal, max_datapoints) {
         if (isNaN(yVal)) {
+            console.log("Arry: " + statArry);
+            console.log("xVal: " + xVal);
+            console.log("yVal: " + yVal);
             console.log('Error! yVal is not a number!');
             return;
         }
         else {
             statObj[statArry].push({
-                label: labelStr,
                 x: xVal,
                 y: yVal
             });
         }
 
-        if (statObj[statArry].length > MAX_DATAPOINTS) {
+        if (statObj[statArry].length > max_datapoints) {
             statObj[statArry].shift();
         };
     };
-
-    updateHolders = function(msgObj, statsObj) {
+    
+    // Function to update stats with data, searches for entry with the same name
+    updateStats = function(msgObj) {
         var timestamp = Date.parse(msgObj.time);
-        console.log('Timestamp: ' + timestamp);
-        // energyTemp += msgObj.power;
+    
+        statID = searchList(statList, "name", msgObj.name);
+    
+        if (statID === -1) {
+            console.log("Error: that entry does not exist!");
+        }
+    
+        try {
+            var lastEntry = statList[statID].energyArry.length - 1
+            var energy = statList[statID].energyArry[lastEntry]["y"] + msgObj.power;
+        } catch (TypeError) {
+            var energy = msgObj.power;
+        }
+    
+        // Update advanced chart data arrays
+        updateStatArry(statList[statID], 'voltageArry', timestamp, msgObj.voltage, MAX_DATAPOINTS_ADV);
+        updateStatArry(statList[statID],'currentArry', timestamp, msgObj.current, MAX_DATAPOINTS_ADV);
+        updateStatArry(statList[statID],'powerArry', timestamp, msgObj.power, MAX_DATAPOINTS_ADV);
+        updateStatArry(statList[statID],'rpmArry', timestamp, msgObj.rpm, MAX_DATAPOINTS_ADV);
+        updateStatArry(statList[statID],'energyArry', timestamp, energy, MAX_DATAPOINTS_ADV);
 
-        // updateStatArry(statsObj, 'voltageArry', 'Voltage', timeVal, msgObj.voltage);
-        // updateStatArry(statsObj,'currentArry', 'Current', timeVal, msgObj.current);
-        updateStatArry(statsObj,'powerArry', 'Handcrank', timestamp, msgObj.power);
-        // updateStatArry(statsObj,'energyArry', 'Energy', timeVal, energyTemp);
-    
-        // timeVal++;
-    
-        // $(voltageHolder).text(msgObj.voltage);
-        // $(currentHolder).text(msgObj.current);
-        // $(powerHolder).text(msgObj.power);
-        // $(rpm0Holder).text(msgObj.rpm0);
-        // $(rpm1Holder).text(msgObj.rpm1);
-        // $(rpm2Holder).text(msgObj.rpm2);
-    
+        // Update basic chart data arrays
+        updateStatArry(statList[statID],'powerArryBasic', timestamp, msgObj.power, MAX_DATAPOINTS_BASIC);
+
+        // $('.stat #voltage').text(msgObj.voltage + " V");
+        // $('.stat #current').text(msgObj.current + " A");
+        // $('.stat #power').text(msgObj.power + " W");
+        // $('.stat #rpm').text(msgObj.rpm + " RPM");
+        // $('.stat #energy').text(energy + " J");
+
         renderAllCharts();
-    };
+    }
 
     renderAllCharts();    
 });
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Energy Demonstration System', page: 'index'});
-// });
-
+/**
+ * =================================
+ *  HOME PAGE
+ * =================================
+ */
 router.get('/', function(req, res, next) {
-  res.render('combined', { title: 'Energy Demonstration System', page: 'combined'});
+    res.render('index', { title: 'Energy Demonstration System', page: 'index'});
 });
 
-router.get('/settings', function(req, res, next) {
-  res.render('settings', { title: 'Energy Demonstration System', page: 'settings'});
+/**
+ * =================================
+ *  LOGIN
+ * =================================
+ */
+// get login page
+// router.get('/login', function(req, res, next){
+//    res.render('login', { message: req.flash('loginMessage') });
+// });
+
+// process login requests
+// router.post('/login', do passport stuff here);
+
+/**
+ * =================================
+ *  SETTINGS
+ * =================================
+ */
+router.get('/settings', isLoggedIn, function(req, res, next) {
+    res.render('settings', { title: 'Energy Demonstration System', page: 'settings'});
 });
 
-/* GET advanced view. */
-// router.get('/advanced', function(req, res, next) {
-//   res.render('advanced', { title: 'Energy Demonstration System - Advanced View', page: 'advanced'});
-// });
+/**
+ * =================================
+ *  LOGOUT
+ * =================================
+ */
+router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+})
 
-/* GET python data */
-// router.get('/readSensor', function(req, res, next) {
-  
-// });
+// router middleware to check if user authenticated
+function isLoggedIn(req, res, next) {
+    // if user authenticated, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // otherwise redirect to login page
+    res.redirect('/login');
+}
 
 module.exports = router;
