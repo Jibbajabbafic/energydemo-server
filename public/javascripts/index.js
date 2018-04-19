9e5c116a09e6866ae6ffb1bdd4bd2608b932b31d
Jibbajabba <Jibbajabbafic@users.noreply.github.com>
2018-04-19 23:35:29 +0100

    Fixed temp display

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -52,16 +52,16 @@ function fancyTimeFormat(time) {
     // Hours, minutes and seconds
     var hrs = ~~(time / 3600);
     var mins = ~~((time % 3600) / 60);
-    var secs = time % 60;
+    var secs = Math.round(time % 60);
 
     // Output like "1:01" or "4:03:59" or "123:03:59"
     var ret = "";
 
     if (hrs > 0) {
-        ret += "" + hrs + " hrs " + (mins < 10 ? "0" : "");
+        ret += "" + hrs + "hrs " + (mins < 10 ? "0" : "");
     }
 
-    ret += "" + mins + " mins " + (secs < 10 ? "0" : "");
+    ret += "" + mins + "mins " + (secs < 10 ? "0" : "");
     ret += "" + secs + "s ";
     return ret;
 };
@@ -84,7 +84,7 @@ socket.on('temps', function(data) {
     console.log('Received temps: ');
     console.log(data);
 
-    $('span#amount').text(data.amount);
+    $('span#amount').text(data.amount*1000);
     $('span#target').text(data.target);
     $('span#ambient').text(data.ambient);
     $('span#kettle').text(data.kettle);
